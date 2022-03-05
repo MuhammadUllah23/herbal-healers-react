af589249c2141bd6d440ce796df3db32ce0ba8e4
Muhammad <muhammadhullah23@gmail.com>
2022-03-05 16:10:45 -0500

    ElevationScroll function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,22 @@ import { useTheme } from "@mui/material";
 import { useScrollTrigger } from "@mui/material";
 import { Typography } from "@mui/material";
 
+function ElevationScroll(props) {
+     
+    const { children } = props;
+    // useScrollTrigger is an event listener
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,   
+      threshold: 0
+    //   how far a user needs to scroll to trigger event listener. Default is 100.
+    });
+    
+    // returns new version of component that is being wrapped by ElevationScroll. Clones the children and returns a new copy of the elevation when triggered
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+}
+
 const useStyles = makeStyles(theme => ({
 
 }))
